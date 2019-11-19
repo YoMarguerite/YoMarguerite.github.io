00c27677ea16ca1eef3717b508fdb2831e22c791
Yoann MARGUERITE <yoann.marguerite@epsi.fr>
2019-11-19 15:20:27 +0100

    init

---
+++ precache-manifest.f7f65d5a62c21a73a391c2ee6ea394ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421b88392a3d5ea4eab5",
+    "url": "/css/chunk-vendors.528f884b.css"
+  },
+  {
+    "revision": "b543de49218b418775b0d59c26f9a210",
+    "url": "/index.html"
+  },
+  {
+    "revision": "436070975706f2d3e132",
+    "url": "/js/app.a33da21d.js"
+  },
+  {
+    "revision": "421b88392a3d5ea4eab5",
+    "url": "/js/chunk-vendors.e7a1155c.js"
+  },
+  {
+    "revision": "a8c976ce1df196bdf9bbe5f3cfb86bee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
